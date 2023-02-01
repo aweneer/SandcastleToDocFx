6376becbe345a99430a3f8fd11132be8e2442012
Jan Hlavac <jan@postsharp.net>
2023-02-01 14:58:09 +0100

    Added CodeEntityReferenceE normalizing and punctuation seeking utilities.

--- src/Utilities.cs
+++ src/Utilities.cs
@@ -23,5 +23,40 @@ namespace SandcastleToDocFx
         {
             return string.Join(" ", originalText.Split(Array.Empty<char>(), StringSplitOptions.RemoveEmptyEntries).ToList().Select(word => word));
         }
+
+        public static bool CheckTextForStartsWithPunctuation(string text)
+        {
+            var punctuationSymbols = new[] {',', '.', ';', ':'};
+            return punctuationSymbols.Any(symbol => symbol.Equals(text[0]));
+        }
+
+        public static string NormalizeCodeEntityReference(string originalReference)
+        {
+            string cleanCodeEntityReference = originalReference;
+            
+            var types = new[] {"E:", "M:", "N:", "P:", "R:", "T:"};
+            var hasType = types.Any(symbol => symbol.Contains(cleanCodeEntityReference[0]));
+            var type = types.FirstOrDefault(symbol => symbol.Contains(cleanCodeEntityReference[0]));
+
+            // Clean reference of type.
+            if (hasType)
+            {
+                cleanCodeEntityReference = originalReference.Remove(0, type!.Length).Trim();
+            }
+
+            // Replace backtick with dash.
+            //cleanCodeEntityReference = cleanCodeEntityReference.Replace("`", "-");
+
+            // Remove method argument.
+            var bracketsPosition = cleanCodeEntityReference.IndexOf('(');
+
+            // if (bracketsPosition != -1)
+            // {
+            //     cleanCodeEntityReference = cleanCodeEntityReference.Remove(bracketsPosition);
+            // }
+
+            return cleanCodeEntityReference;
+        }
+
     }
 }
