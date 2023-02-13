4bfad38fa1a9e2aa8d7e05e5e7a2f284d62bf6dc
Jan Hlavac <jan@postsharp.net>
2023-02-13 12:28:01 +0100

    Added export of Sandcastle Help File Builder file to markdown.

--- src/Program.cs
+++ src/Program.cs
@@ -19,6 +19,7 @@ namespace SandcastleToDocFx
         public static DirectoryInfo? DocumentationFilesDirectory;
         public static XElement? CurrentConceptualFileRootElement;
         public static FileInfo? CurrentConceptualFile;
+        public static DirectoryInfo? ExportDestinationDirectory;
         static void Main(string[] args)
         {
             // TODO: Finish commandline app.
@@ -37,18 +38,18 @@ namespace SandcastleToDocFx
                 Directory.CreateDirectory(exportDirectory);
             }
             
-            var destination = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "SandCastleToDocFx_Export",
-                "conceptual");
+            ExportDestinationDirectory = new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "SandCastleToDocFx_Export",
+                "conceptual"));
             
-            if (Directory.Exists(destination))
+            if (Directory.Exists(ExportDestinationDirectory.FullName))
             {
-                Directory.Delete(destination, true);
+                Directory.Delete(ExportDestinationDirectory.FullName, true);
             }
 
-            Directory.CreateDirectory(destination);
+            Directory.CreateDirectory(ExportDestinationDirectory.FullName);
             
             
-            var destinationDirectoryInfo = new DirectoryInfo(destination);
+            var destinationDirectoryInfo = new DirectoryInfo(ExportDestinationDirectory.FullName);
             var sourceDirectoryInfo = new DirectoryInfo("C:\\src\\PostSharp.Documentation\\Source");
             DocumentationFilesDirectory = sourceDirectoryInfo;
             SourceCodeDirectory = "C:\\src\\PostSharp.Documentation\\Samples";
@@ -64,13 +65,53 @@ namespace SandcastleToDocFx
                 //yamlWriter.CreateRootToc(topic, yamlWriter.SourceDirectory, true);
             }
 
-            yamlWriter.WriteYamlFile(Path.Combine(destination, "toc.yml"));
+            yamlWriter.WriteYamlFile(Path.Combine(ExportDestinationDirectory.FullName, "toc.yml"));
             
             
             // Start processing .aml files with visitor.
             var visitor = new MamlVisitor();
-            TransformMamlFilesToMarkdown(visitor, sourceDirectoryInfo, destination);
-            Console.WriteLine($"Exporting .aml files to .md completed in '{destination}'.");
+            TransformMamlFilesToMarkdown(visitor, sourceDirectoryInfo, ExportDestinationDirectory.FullName);
+            Console.WriteLine($"Exporting .aml files to .md completed in '{ExportDestinationDirectory.FullName}'.");
+
+            var shfbProjectFile = Path.Combine(DocumentationFilesDirectory.FullName, "PostSharp.shfbproj");
+            Console.WriteLine($"Exporting .shfbproj file '{shfbProjectFile}' to .md.");
+            TransformSandcastleHelpFileBuilderProject(shfbProjectFile);
+            
+        }
+
+        public static void TransformSandcastleHelpFileBuilderProject(string projectFile)
+        {
+            var document = XDocument.Load(projectFile);
+            var rootNamespaceTitle = document.Descendants().FirstOrDefault( e => e.Name.LocalName == "RootNamespaceTitle")?.Value;
+            var projectSummary = document.Descendants().FirstOrDefault( e => e.Name.LocalName == "ProjectSummary")?.Value;
+
+            var namespaceSummariesElement = document.Descendants().FirstOrDefault( e => e.Name.LocalName == "NamespaceSummaries");
+
+            MarkdownWriter.StartMarkdownMetadata();
+            MarkdownWriter.AppendMetadataUid("Project_PostSharp");
+            MarkdownWriter.AppendMetadataTitle(rootNamespaceTitle);
+            MarkdownWriter.AppendMetadata("product", "postsharp");
+            MarkdownWriter.AppendMetadata("categories", new[] { "PostSharp", "AOP", "PostSharp API" });
+            MarkdownWriter.EndMarkdownMetadata();
+            
+            MarkdownWriter.WriteHeading1(rootNamespaceTitle);
+            MarkdownWriter.AppendLine(projectSummary);
+            MarkdownWriter.WriteHeading2("Namespaces");
+            MarkdownWriter.AppendTableHeader(new[]{"Namespace", "Description"});
+
+            var documentedNamespaces = namespaceSummariesElement.Elements().Where(e => e.Attribute("isDocumented").Value == "True");
+            
+            foreach (var namespaceSummaryElement in documentedNamespaces)
+            {
+                var namespaceName = namespaceSummaryElement.Attribute("name").Value;
+                MarkdownWriter.StartTableRow();
+                MarkdownWriter.WriteXref(namespaceName);
+                MarkdownWriter.AddTableRowSeparator();
+                MarkdownWriter.Append(namespaceSummaryElement.Value);
+                MarkdownWriter.EndTableRow();
+            }
+            
+            MarkdownWriter.WriteFile(ExportDestinationDirectory.FullName, "Project_PostSharp");
         }
 
         public static void TransformMamlFilesToMarkdown(MamlVisitor visitor, DirectoryInfo sourceDirectory, string destinationDirectory)
@@ -109,7 +150,10 @@ namespace SandcastleToDocFx
                     .Select(e => e.Attribute("title")?.Value ?? "null")
                     .SingleOrDefault();
 
-                MarkdownWriter.AppendMetadataTitle(fileTitle);
+                MarkdownWriter.AppendMetadata("title", fileTitle);
+                MarkdownWriter.AppendMetadata("product", "postsharp");
+                MarkdownWriter.AppendMetadata("categories", new[] { "PostSharp", "AOP", "Metaprogramming" });
+                //MarkdownWriter.AppendMetadataTitle(fileTitle);
                 MarkdownWriter.EndMarkdownMetadata();
                 
                 // Add heading for the article based on title from .aml file.
